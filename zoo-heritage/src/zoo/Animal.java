7a723a59f6fa57b766aaa4e378289a897cf526af
anisskiller <anissaallou36@gmail.com>
2018-11-20 12:40:57 +0100

    Animal

---
+++ zoo-heritage/src/zoo/Animal.java
@@ -0,0 +1,13 @@
+package zoo;
+
+public class Animal {
+	 
+	public String nom;
+	public String type; 
+	 
+	public Animal(String nom, String type) {
+		this.nom = nom; 
+		this.type = type; 
+	}
+
+}
